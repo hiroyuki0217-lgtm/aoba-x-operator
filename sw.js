78abbfced82175262efab99dd388eba07876de64
hiroyuki0217-lgtm <hiroyuki0217@gmail.com>
2026-08-13 12:17:03 +0900

    Refresh web app cache

--- sw.js
+++ sw.js
@@ -1,2 +1,5 @@
-self.addEventListener('install', event => event.waitUntil(caches.open('aoba-v1').then(cache => cache.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg']))));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request))));
+const CACHE = 'aoba-v2';
+const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => event.respondWith(fetch(event.request).then(response => response).catch(() => caches.match(event.request))));
